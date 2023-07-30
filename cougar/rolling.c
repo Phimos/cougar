16b24393fbd7ac87d29844725e8fb1dd8d376a43
甘云冲 <ganyunchong@ganyunchongs-MacBook-Pro.local>
2023-07-30 22:51:41 +0800

    <refactor> split

--- cougar/rolling.c
+++ cougar/rolling.c
@@ -24,17 +24,21 @@
 static PyMethodDef rolling_methods[] = {
     {"rolling_sum", (PyCFunction)rolling_sum, METH_VARARGS | METH_KEYWORDS, rolling_sum_doc},
     {"rolling_mean", (PyCFunction)rolling_mean, METH_VARARGS | METH_KEYWORDS, "Rolling mean"},
+
     {"rolling_std", (PyCFunction)rolling_std, METH_VARARGS | METH_KEYWORDS, "Rolling std"},
     {"rolling_var", (PyCFunction)rolling_var, METH_VARARGS | METH_KEYWORDS, "Rolling var"},
     {"rolling_skew", (PyCFunction)rolling_skew, METH_VARARGS | METH_KEYWORDS, "Rolling skew"},
     {"rolling_kurt", (PyCFunction)rolling_kurt, METH_VARARGS | METH_KEYWORDS, "Rolling kurt"},
+
     {"rolling_max", (PyCFunction)rolling_max, METH_VARARGS | METH_KEYWORDS, "Rolling max"},
     {"rolling_min", (PyCFunction)rolling_min, METH_VARARGS | METH_KEYWORDS, "Rolling min"},
     {"rolling_argmin", (PyCFunction)rolling_argmin, METH_VARARGS | METH_KEYWORDS, "Rolling argmin"},
     {"rolling_argmax", (PyCFunction)rolling_argmax, METH_VARARGS | METH_KEYWORDS, "Rolling argmax"},
+
     {"rolling_median", (PyCFunction)rolling_median, METH_VARARGS | METH_KEYWORDS, "Rolling median"},
     {"rolling_rank", (PyCFunction)rolling_rank, METH_VARARGS | METH_KEYWORDS, "Rolling rank"},
     {"rolling_quantile", (PyCFunction)rolling_quantile, METH_VARARGS | METH_KEYWORDS, "Rolling quantile"},
+
     {NULL, NULL, 0, NULL}};
 
 /* Module structure */
@@ -47,8 +51,7 @@ static struct PyModuleDef rolling_module = {
 };
 
 /* Module initialization function */
-PyMODINIT_FUNC
-PyInit_rolling(void) {
+PyMODINIT_FUNC PyInit_rolling(void) {
     PyObject* module = PyModule_Create(&rolling_module);
     import_array();
     return module;
